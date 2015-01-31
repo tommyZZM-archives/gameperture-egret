77a6eddabde37461438ceae19e29c34dc86b9afc
'tommyZZM' <452490446@qq.com>
2015-01-31 18:06:04 +0800

    gitgit

--- src/vendor/gameperture/core/bootstrap/GameLauncher.ts
+++ src/vendor/gameperture/core/bootstrap/GameLauncher.ts
@@ -4,9 +4,11 @@ module gamep{
         private _facade:GameFacade = GameFacade.instance;
 
         public constructor(debug:boolean) {
-            console.info("Welcome to %cGameperture","color:#1ac2ff;font-weight:bold;",
-                "Quick Game Devlope Template base on Egret Engine!");
-            console.info("gitHub:",'https://github.com/tommyZZM/gameperture-egret')
+            if(debug){
+                console.info("Welcome to %cGameperture","color:#1ac2ff;font-weight:bold;",
+                    "Quick Game Devlope Template base on Egret Engine!");
+                console.info("gitHub:",'https://github.com/tommyZZM/gameperture-egret');
+            }
             isdebug = debug;
             if(isdebug){
                 //egret.Profiler.getInstance().run();
